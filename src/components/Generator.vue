ee5a83a6a921465b65da16eecdde59360517567a
Evan Grosset-Janin <evan.grosset@icloud.com>
2024-11-23 15:27:24 +0100

    Initialize project

---
+++ src/components/Generator.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" :value="password"/>
+  <button @click="generate">Generate Password</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      password: 'password'
+    }
+  },
+  methods: {
+    generate () {
+      this.password = Math.random().toString(36).slice(-8)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
